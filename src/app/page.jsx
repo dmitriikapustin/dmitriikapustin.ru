fe14c0c190755101d9ebd93788d4d3f8046ff87f
Dmitrii Kapustin <dmitriikapustin@ya.ru>
2024-10-09 14:39:15 +0300

    гзв

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,13 +9,13 @@ export default function Page() {
         <h1>
           <strong>Dmitrii Kapustin</strong>
           <br/>
-          <small style={{fontWeight: 400, opacity: 0.8}}>Products</small>
+          <small style={{fontWeight: 400, opacity: 0.8}}>IT enterpreneur</small>
         </h1>
       </div>
       <div className="col-2">
         <div className="text">
-          <p><strong>Lorem ipsum dolor</strong> sit amet consectetur adipisicing elit. Architecto laboriosam quas harum, obcaecati voluptate nostrum enim vel omnis! Laudantium dicta, quos quibusdam odio veritatis labore nam! Sunt similique perferendis maxime.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laboriosam quas harum, obcaecati voluptate nostrum enim vel omnis! Laudantium dicta, quos quibusdam odio veritatis labore nam! Sunt similique perferendis maxime.</p>
+          <p><strong>12 years experience</strong> in software development, design, creative and product launches.</p>
+          <p>Since 2013 I have been working in the IT industry in UI/UX, graphic design, Full-stack development, creative concepts and products development.</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laboriosam quas harum, obcaecati voluptate nostrum enim vel omnis! Laudantium dicta, quos quibusdam odio veritatis labore nam! Sunt similique perferendis maxime.</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laboriosam quas harum, obcaecati voluptate nostrum enim vel omnis! Laudantium dicta, quos quibusdam odio veritatis labore nam! Sunt similique perferendis maxime.</p>
         </div>
